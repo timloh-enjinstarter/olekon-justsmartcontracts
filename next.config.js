96db952680b0682a32afcea78afeb7fe4654c87d
Tim Loh <timloh@Tims-MacBook-Pro.local>
2024-01-06 04:56:12 +0800

    build: enable static export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
